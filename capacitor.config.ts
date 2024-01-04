aa3b958982174007febd60731eb40345d06cc328
Randy <pointdexter3949@gmail.com>
2024-01-04 16:20:13 -0500

    minor changes, add capacitor android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AppFlow POC - from template',
+  appId: 'com.randy.beard.bmo.app-flow-poc-from-template',
+  appName: 'App Flow POC from Template',
   webDir: 'www',
-  server: {
-    androidScheme: 'https'
+  plugins: {
+    LiveUpdates: {
+      appId: '2da538c9',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
   }
 };
 
