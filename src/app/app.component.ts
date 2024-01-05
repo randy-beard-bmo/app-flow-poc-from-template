f0f6777c1870a20562d6a8367ba3a5dccffbed14
Randy <pointdexter3949@gmail.com>
2024-01-05 13:53:53 -0500

    add debug logs, bump web 2.0.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ import * as LiveUpdates from '@capacitor/live-updates';
 export class AppComponent implements OnInit {
   constructor() {}
 
-
   ngOnInit(): void {
     this.initializeApp();
   }
@@ -19,7 +18,24 @@ export class AppComponent implements OnInit {
   async initializeApp() {
     // Register event to fire each time user resumes the app
     App.addListener('resume', async () => {
-      if (localStorage['shouldReloadApp'] && localStorage['sessionActive'] === false) {
+      console.log(
+        'randy - local storage type',
+        typeof localStorage['shouldReloadApp'],
+        typeof localStorage['sessionActive'],
+        '\n',
+        localStorage['shouldReloadApp'] === 'true',
+        '\n',
+        localStorage['shouldReloadApp'] === 'false',
+        '\n',
+        localStorage['sessionActive'] === 'true',
+        '\n',
+        localStorage['sessionActive'] === 'false',
+
+      );
+      if (
+        localStorage['shouldReloadApp'] === 'true' &&
+        localStorage['sessionActive'] === 'false'
+      ) {
         await LiveUpdates.reload();
       } else {
         const result = await LiveUpdates.sync();
